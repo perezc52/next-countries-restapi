e2cc5024d5c8c6c4a2f682c0102bf79452a1f21e
Carlos Perez <pcarlos132@yahoo.com>
2024-07-21 20:54:40 -0500

    suspense

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'flagcdn.com',
-            },
-            {
-                protocol: 'https',
-                hostname: 'upload.wikimedia.org'
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flagcdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+    ],
+  },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 };
 
 export default nextConfig;
